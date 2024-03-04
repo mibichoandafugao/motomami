e55f09f2841ec50f63e03cc2c63963bf9c131b63
mibichoandafugao <augustohenao1234@gmail.com>
2024-03-03 21:32:35 -0500

    Files Updated

---
+++ src/components/NavGlobal.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/NavGlobal.css'
+
+export function NavGlobal() {
+
+
+    return (
+
+        <div className='container'>
+            <nav className='navbar'>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/InDrive_Logo.svg/2560px-InDrive_Logo.svg.png ' alt='#'></img>
+                <button><p>Iniciar Sesion</p></button>
+            </nav>
+        </div>
+    )
+}
